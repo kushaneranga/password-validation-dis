c2c9c9402ff4e9058782351d8e922e5f6f74c60c
Kushan <kushaneranga>
2022-12-26 19:25:04 +0530

    massages fun update

--- src/js/massages.js
+++ src/js/massages.js
@@ -1,12 +1,20 @@
+module.exports = function (msgType, data = null, fnTrue = false) {
+  return (fnTrue && requirements[msgType](data)) || requirements[msgType];
+};
+
 const requirements = {
-    length: (charLgth) => `Make your password at least ${charLgth} characters long`,
-    letters: 'A mixture of both uppercase and lowercase letters',
-    numbers: 'A mixture of letters and numbers',
-    specialCharacter: 'Inclusion of at least one special character',
-    common: 'Avoid using common words or phrases',
-    personalInformation: 'Avoid using personal information',
-    password: 'Password should be a valid string',
-    minMaxLNumbers: (minLgth, maxLgth, num) => `The string should have a minimum Num length of ${minLgth} & maximum Num length of ${maxLgth} and you number length is ${num.length}`,
-    minLNumbers: (lgth, num) => `The string should have a minimum Num length of ${lgth} and you number length is ${num.length}`,
-    maxLNumbers: (lgth, num) => `The string should have a maximum Num length of ${lgth} and you number length is ${num.length}`,
-}
+  length: (charLgth) =>
+    `Make your password at least ${charLgth} characters long`,
+  letters: "A mixture of both uppercase and lowercase letters",
+  numbers: "A mixture of letters and numbers",
+  specialCharacter: "Inclusion of at least one special character",
+  common: "Avoid using common words or phrases",
+  personalInformation: "Avoid using personal information",
+  password: "Password should be a valid string",
+  minMaxLNumbers: (minLgth, maxLgth, num) =>
+    `The string should have a minimum Num length of ${minLgth} & maximum Num length of ${maxLgth} and you number length is ${num.length}`,
+  minLNumbers: (lgth, num) =>
+    `The string should have a minimum Num length of ${lgth} and you number length is ${num.length}`,
+  maxLNumbers: (lgth, num) =>
+    `The string should have a maximum Num length of ${lgth} and you number length is ${num.length}`,
+};
